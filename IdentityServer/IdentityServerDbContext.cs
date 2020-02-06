678397a4c53277e141cc4803c397d8f7f9acbb05
Alex Cherkashyn <alexander.cherkashyn@altusnova.com>
2020-02-06 11:08:10 +0300

    Init IdentityServer4

---
+++ IdentityServer/IdentityServerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace IdentityServer
+{
+    public class IdentityServerDbContext : ApiAuthorizationDbContext<IdentityUser>
+
+    {
+        public IdentityServerDbContext(
+            DbContextOptions<IdentityServerDbContext> options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
