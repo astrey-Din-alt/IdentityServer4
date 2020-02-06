cbba90e03ecb5735fddde537899bb02f2356b25d
Alex Cherkashyn <alexander.cherkashyn@altusnova.com>
2020-02-06 13:53:30 +0300

    Add Config and update Startup;

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "spa",
+                    ClientName = "SinglePage",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+                    RequireConsent = false,
+                    AccessTokenLifetime = 120,
+
+                    RedirectUris = {
+                        "http://localhost:3000/#/callback",
+                        "http://localhost:3000/silentRenew.html",
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "http://localhost:44300/account/login"
+                    },
+                    AllowedCorsOrigins = { "http://localhost:3000" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    },
+                }
+            };
+        }
+    }
+}
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,9 +30,49 @@ namespace IdentityServer
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<IdentityServerDbContext>();
+                .AddEntityFrameworkStores<IdentityServerDbContext>()
+                .AddDefaultTokenProviders();
 
             services.AddRazorPages();
+
+            services.AddIdentityServer(options =>
+                {
+                    // http://docs.identityserver.io/en/release/reference/options.html#refoptions
+                    options.Endpoints = new EndpointsOptions
+                    {
+                        // в Implicit Flow используется для получения токенов
+                        EnableAuthorizeEndpoint = true,
+                        // для получения статуса сессии
+                        EnableCheckSessionEndpoint = true,
+                        // для логаута по инициативе пользователя
+                        EnableEndSessionEndpoint = true,
+                        // для получения claims аутентифицированного пользователя
+                        // http://openid.net/specs/openid-connect-core-1_0.html#UserInfo
+                        EnableUserInfoEndpoint = true,
+                        // используется OpenId Connect для получения метаданных
+                        EnableDiscoveryEndpoint = true,
+
+                        // для получения информации о токенах, мы не используем
+                        EnableIntrospectionEndpoint = false,
+                        // нам не нужен т.к. в Implicit Flow access_token получают через authorization_endpoint
+                        EnableTokenEndpoint = false,
+                        // мы не используем refresh и reference tokens
+                        // http://docs.identityserver.io/en/release/topics/reference_tokens.html
+                        EnableTokenRevocationEndpoint = false
+                    };
+
+                    // IdentitySever использует cookie для хранения своей сессии
+                    options.Authentication = new IdentityServer4.Configuration.AuthenticationOptions
+                    {
+                        CookieLifetime = TimeSpan.FromDays(1)
+                    };
+                })
+                .AddDeveloperSigningCredential()
+                .AddInMemoryPersistedGrants()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddAspNetIdentity<IdentityUser>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +95,10 @@ namespace IdentityServer
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseStaticFiles();
+
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
